12575a1ac7a7fd0d571cac2064a67718e98ef0c2
Aaron <aaronlamfinance@gmail.com>
2023-07-15 12:08:17 -0700

    Create starter pseudocode

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,24 @@
+//Variables 
 var gasKey = "WeUe2S9Wb1CvTZt1wVPAi7J3CvEuzPwpRT0w4N7y";
 var gasUrl = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?&api_key=WeUe2S9Wb1CvTZt1wVPAi7J3CvEuzPwpRT0w4N7y&fuel_type=BD,RD&access=public";
 
 var searchInput; //jquery to input div later
 
 // added gmaps API + fetch tests below.
-
 var gmapsKey = "&key=AIzaSyAEixeDUTRcNwCXOgNXbeiS2yd-F6g4SZY"; 
-var address = "1600 Ampihitheatre Parkway, Mountain View, CA";
 var geocode = "https://maps.googleapis.com/maps/api/geocode/json?address="+ address + gmapsKey;
-
-var lat = "40.714224";
-var long = "-73.961452";
-
 var reverseGeocode = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + long + gmapsKey;
 
-var address = "1600 Ampihitheatre Parkway, Mountain View, CA"
+
+
+
+//APIS
+
+//Javascript = Our code manipulating the web
+//Third-Party = others manipulating our code (HTML)
+//Server Side APIs = document.getElementById
 
 console.log(gasUrl);
-
-
 fetch(gasUrl)
 .then(function (response) {
     return response.json();
@@ -35,7 +35,7 @@ fetch(geocode)
    .then(function (data) {
    console.log(data);
    });
-
+ 
 fetch(reverseGeocode)
 .then(function(response) {
    return response.json();
@@ -43,3 +43,37 @@ fetch(reverseGeocode)
    .then(function(data) {
       console.log(data);
    })
+
+
+//function 
+
+x+6=11 
+x
+
+function() {
+   
+   var x;
+   = console.log("banana cream")
+}
+
+function()
+
+
+
+
+
+
+
+
+
+//event listeners
+
+
+/*
+Variables
+   Conditionals 
+      Loops 
+         Functions -
+            Objects - Other Objects / Nouns 
+               Arrays - Values 
+               */
