c94afc0ba51da269c782fd86457f411626af21ee
Aaron <aaronlamfinance@gmail.com>
2023-07-17 03:50:44 -0700

    tested changes to city input

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@
 /* Variables Below
 ___________________________________________*/
 var gasKey = "WeUe2S9Wb1CvTZt1wVPAi7J3CvEuzPwpRT0w4N7y";
-var gasUrl = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?&api_key=WeUe2S9Wb1CvTZt1wVPAi7J3CvEuzPwpRT0w4N7y&access=public";
+var gasApiUrl = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?&api_key=WeUe2S9Wb1CvTZt1wVPAi7J3CvEuzPwpRT0w4N7y&access=public";
 
 //fuel_type=BD,RD&access=public";
 
@@ -14,9 +14,9 @@ var gmapsKey = "&key=AIzaSyAEixeDUTRcNwCXOgNXbeiS2yd-F6g4SZY";
 var geocodeLink = "https://maps.googleapis.com/maps/api/geocode/json?address=";
 var reverseGeoLink = "https://maps.googleapis.com/maps/api/geocode/json?latlng=";
 
-var formContainerEl = document.getElementById("search-form");
-var cityInputEl = document.getElementById("city-input");
-var fuelTypeInputEl = document.getElementById("fuel-type-input");
+var formEl = document.getElementById("search-form");
+var cityInputEl = document.getElementById('search-input');
+var fuelTypeInputEl = document.getElementById("fuel-type");
 var btnEl = document.getElementById("search-button"); 
 
 /*Functions Below
@@ -30,12 +30,13 @@ ___________________________________________*/
 function handleUserInput(event) {
    event.preventDefault()
    var fuelTypeInput = fuelTypeInputEl.value;
-   runApi(fuelTypeInput);
+   var cityInput = cityInputEl.value;
+   runFuelApi(fuelTypeInput,cityInput);
 }
 
-function runApi(fuelType) {
-  var apiURL = gasUrl + "&fuel_type=" + fuelType 
-  fetch(apiURL)
+function runFuelApi(fuelType,city) {
+  var urlFuel = gasApiUrl + "&fuel_type=" + fuelType + "&city=" + city + "&limit=5"
+  fetch(urlFuel)
   .then(function (response) {
      return response.json();
   })
@@ -45,16 +46,18 @@ function runApi(fuelType) {
   })
 }
 
-/*  Explaining Previous code.
-If you check line 78, that line is after the event.preventDefault.  That's why it keeps resetting.   However, that line is included in the runApi function, which is why it ran. 
+// function runCityApi(city) {
+//    var cityUrl = gasApiUrl + "&city=" + city + "&limit=5";
+//    fetch(cityUrl)
+//    .then(function (response) {
+//       return response.json();
+//    })
+//    .then(function (data) {
+//       console.log(data);
+//    })
+//  }
 
-Before, we only had one event listener.  the only event being listened to was the button. Once I added the fuelTypeInput to the event listeners, now it prevents default and saves the new values. 
 
-This does create the issue of constantly runningApi when you click the dropdown menu, as now the eventListener listens to every event of the input, but it works fine, so I'm keeping it for now.
--Aaron 7/16
-*/
-
-// potential Search function -- This might require another API. Yes, we can locate the gas stations, as the city parameter is part of the gas API. However, we need to use the mapping API to get to that location. Hopefully google as an easy API for getting your current location.  -AL 7/16
 function searchGasStation(){
   var cityInput=document.getElementById('user-input').value;
    var fuelTypeInput=document.getElementById('fuel-type-input').value; 
@@ -62,7 +65,8 @@ function searchGasStation(){
 // need to add more to this function this is just a start (TM-07/15)
 
 
-findGasStations(cityInput,fuelTypeInput) //This function is taking in the city and fuel type parameters. in this function we will use API to FETCH the gas station data. 
+findGasStations(cityInput,fuelTypeInput) 
+//This function is taking in the city and fuel type parameters. in this function we will use API to FETCH the gas station data. 
 }
 
 function findGasStations(city, fuelTypeInput){
@@ -76,41 +80,31 @@ function displayGastStations(gasStations){
   resultsContainer.innerHTML= '';//
 }
 
-/* Test FUNCTIONS
-_____________________________*/
-
-// function handleUserInput(event) {
-//    event.preventDefault()
-//    var formContainerEl = document.getElementById("search-form");
-
-//    testRunApi(formContainerEl);
-// }
-
-// function testRunApi(city) {
-//    var apiURL = gasUrl + "&city=" + city; 
-
-//    fetch(apiURL)
-//    .then(function (response) {
-//       return response.json();
-//    })
-//    .then(function (data) {
-//       console.log(data);
-//    })
-//  }
-
 /*Event Listeners Below
 _____________________________________________________________*/
-//formContainerEl.addEventListener("submit", handleUserInput) 
 
-//formContainerEl.addEventListener("click", handleUserInput);
-btnEl.addEventListener("click", handleUserInput); 
-fuelTypeInputEl.addEventListener("click",handleUserInput);
-cityInputEl.addEventListener("", handleUserInput);
+formEl.addEventListener("submit", handleUserInput);
+
+
+
+//The event listener above uses <form>. This allows everything inside the form container to be listened to. 
+// btnEl.addEventListener("click", handleUserInput); 
+// fuelTypeInputEl.addEventListener("click",handleUserInput);
+// cityInputEl.addEventListener("", handleUserInput);
 
      
 /* Comments
 _______________________________________________________________*/
+/*  Explaining Previous code.
+If you check line 78, that line is after the event.preventDefault.  That's why it keeps resetting.   However, that line is included in the runApi function, which is why it ran. 
 
+Before, we only had one event listener.  the only event being listened to was the button. Once I added the fuelTypeInput to the event listeners, now it prevents default and saves the new values. 
+
+This does create the issue of constantly runningApi when you click the dropdown menu, as now the eventListener listens to every event of the input, but it works fine, so I'm keeping it for now.
+-Aaron 7/16
+*/
+
+// potential Search function -- This might require another API. Yes, we can locate the gas stations, as the city parameter is part of the gas API. However, we need to use the mapping API to get to that location. Hopefully google as an easy API for getting your current location.  -AL 7/16
 /*the code under here is not necessary. All this function does is add parameters to the URL. Once the parameters are added properly, then we can search for specific things. What happens is your drop down menu adds the search parameter ("&fuel_type="... BD/ELEC/RD/LPG)... Now, we need to add the city parameters. The hard part might be the 2nd api, which will get the location of the user and map them to the gas station. That might be easy though...? 
 -Aaron 7/16
 */
